dc1edf042dcb03ead82cf58d10db6dca9ad7e04b
naaiyy <naaiyyyy@gmail.com>
2025-05-23 16:36:56 +0200

    Reduces padding and gap in main container
    Reduces the padding and gap within the main container to create a more compact layout.
    
    This adjustment improves the visual balance and spacing between elements within the container.

--- frontend/src/components/layout/MainContainer.tsx
+++ frontend/src/components/layout/MainContainer.tsx
@@ -10,7 +10,10 @@ interface Props {
  */
 export default function MainContainer({ children }: Props) {
   return (
-    <div className="flex h-full w-full gap-4 rounded-lg border border-neutral-700 bg-neutral-800/60 p-4 backdrop-blur">
+    /**
+    * Defines margins between panes inside the main container (gap-2 & p-2)
+    * */
+    <div className="flex h-full w-full gap-2 rounded-lg border border-neutral-700 bg-neutral-800/60 p-2 backdrop-blur">
       {children}
     </div>
   );
